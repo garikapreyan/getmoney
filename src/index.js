318852a7918640329f6ee8dae9780220f2af2a67
Garik <garik.apreyan@mail.ru>
2018-11-10 15:58:58 +0400

    [add] store to project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import store from './configureStore';
 
-ReactDOM.render(<div />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><div/></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
